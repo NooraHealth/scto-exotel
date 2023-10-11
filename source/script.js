7f1a16981243edd39b1ec1897ed376822c272e17
Udit <23015406+udit-001@users.noreply.github.com>
2023-10-11 11:47:03 +0530

    Revamped UI and added error handling

--- source/script.js
+++ source/script.js
@@ -2,32 +2,38 @@
 var fromNumber = document.getElementById('fromNumber');
 var toNumber = document.getElementById('toNumber');
 var dialBtn = document.getElementById('dial');
-var exotelResultsValue = document.getElementById('exotelResultsValue');
-var txt1 = document.getElementById('txt1');
-var txt2 = document.getElementById('txt2');
-var txt3 = document.getElementById('txt3');
+var title = document.getElementById('title');
+var hint = document.getElementById('hint');
+var surveyorDetails = document.getElementById('surveyorDetails');
+var buttonHint = document.getElementById('buttonHint');
+var result = document.getElementById('statusBox');
+var reasonDiv = document.getElementById('reasonBox');
+var answerState = document.getElementById("answerState");
 
 
 // References to values stored in the plug-in parameters
-var apikey = getPluginParameter('apikey');
-var apitoken = getPluginParameter('apitoken');
+var apiKey = getPluginParameter('apikey');
+var apiToken = getPluginParameter('apitoken');
 var accountSid = getPluginParameter('accountSid');
 var pfromNumber = getPluginParameter('fromNumber');
 var ptoNumber = getPluginParameter('toNumber');
-var calledID = getPluginParameter('calledID');
+var pcallerId = getPluginParameter('callerId');
 var recording = getPluginParameter('recording');
 var displaynumber = getPluginParameter('displaynumber');
-var type = getPluginParameter('type');
+var baseUrl = getPluginParameter('baseUrl') || "api.exotel.in";
+var type = getPluginParameter('type') || "call";
 var msgBody = getPluginParameter('msgBody');
+var currentAnswer = fieldProperties.CURRENT_ANSWER;
 
 
 // First, show the current values form the fieldParams object
 if (type) {
     if (type === "sms") {
-        txt1.innerText = "Please check before sending SMS:";
-        txt2.style.visibility = 'hidden';
-        dialBtn.innerText = 'SMS';
-        txt3.innerText = "This will send SMS to the respondent.";
+        hint.innerText = "Please check before sending SMS:";
+        surveyorDetails.style.visibility = 'hidden';
+        dialBtn.innerText = 'Send SMS';
+        title.innerText = 'Exotel SMS';
+        buttonHint.innerText = "This will send SMS to the respondent.";
     } else {
         fromNumber.innerHTML = pfromNumber;
     }
@@ -35,16 +41,104 @@ if (type) {
 } else {
     fromNumber.innerHTML = pfromNumber;
 }
+setCurrentStatus();
+
+
+function setResult(resultClass, resultText, reason = null, html = false) {
+    t1 = result.classList.replace("danger", resultClass);
+    t2 = result.classList.replace("success", resultClass);
+
+    if ((t1 || t2) == false) {
+        result.classList.add(resultClass);
+    }
+    result.innerText = resultText;
+    if (reason != null) {
+        reasonDiv.classList.add('reason');
+        if (html == true) {
+            reasonDiv.innerHTML = reason;
+        }
+        else {
+            reasonDiv.innerText = reason;
+        }
+        var metadata = [resultText, reason, new Date().toString(), html];
+        setMetaData(metadata.join(","));
+    }
+}
+
+
+function formatDateTime(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear(),
+        hours = '' + d.getHours(),
+        minutes = '' + d.getMinutes();
+
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+
+    if (month.length < 2)
+        month = '0' + month;
+    if (day.length < 2)
+        day = '0' + day;
+    if (hours.length < 2)
+        hours = '0' + hours;
+    if (minutes.length < 2)
+        minutes = '0' + minutes;
+
+    return [day, month, year].join('-') + ' ' + strTime;
+}
+
+
+function setCurrentStatus() {
+    var stored_metadata = getMetaData() || "";
+    var split_metadata = stored_metadata.split(',');
+    var metadata = {
+        "status": split_metadata[0],
+        "reason": split_metadata[1],
+        "timestamp": formatDateTime(split_metadata[2]),
+        "html": split_metadata[3]
+    };
+    if (metadata != '') {
+        var last_response_time = metadata['timestamp'];
+        if (metadata["status"] == "Success") {
+            setResult("success", "Success", metadata['reason'], metadata['html']);
+            if (last_response_time != undefined) {
+                answerState.innerHTML = "* Last recorded server response at " + last_response_time;
+            }
+        }
+        else if (metadata["status"] == "Failure") {
+            setResult("danger", "Failure", metadata['reason'], metadata['html']);
+            if (last_response_time != undefined) {
+                answerState.innerHTML = "* Last recorded server response at " + last_response_time;
+            }
+        }
+    }
+}
 
 toNumber.innerHTML = ptoNumber;
-if (displaynumber === 0){
-	toNumber.innerHTML = '**********';
+if (displaynumber === 0) {
+    toNumber.innerHTML = '**********';
 }
 
 
 // Define the dial function
 dialBtn.onclick = function () {
-    getSite();
+    apiCall()
+}
+
+function generateAPIUrl(baseUrl, accountSid, type) {
+    var endpoint;
+    if (type == "sms") {
+        endpoint = "/Sms/send.json"
+    }
+    else {
+        endpoint = "/Calls/connect.json"
+    }
+    return "https://" + apiKey + ":" + apiToken + "@" + baseUrl + '/v1/Accounts/' + accountSid + endpoint;
 }
 
 function makeHttpObject() {
@@ -58,6 +152,122 @@ function makeHttpObject() {
     throw new Error("Could not create HTTP request object.");
 }
 
+
+function parseError(data) {
+    msg = data["RestException"]["Message"];
+    if (msg.includes("NDNC")) {
+        return "The number is registered under DND"
+    }
+    return msg;
+}
+
+function processResponse(data) {
+    var status = data["Call"]["Status"] == "in-progress" ? "Success" : "Failure";
+    var statusClass = data['status'] == "success" ? 'success' : 'danger';
+    if (status == "Success") {
+        var sid = data["Call"]["Sid"];
+        setResult(statusClass, status, "Call was placed successfully!")
+        setAnswer(sid);
+        return true;
+    }
+    else if (status == "Failure") {
+        setResult(statusClass, status, "Call couldn't be placed!")
+        return false;
+    }
+}
+
+
+function createCallPayload(data) {
+    var record;
+    if (data["Record"] == 0) {
+        record = false;
+    }
+    else {
+        record = true;
+    }
+    return {
+        "CallerId": "0" + data["CallerId"],
+        "From": "0" + data["From"],
+        "To": data["To"],
+        "Record": record
+    }
+}
+
+function jsonToQueryString(json) {
+    const queryString = Object.keys(json)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(json[key])}`)
+        .join('&');
+    return queryString;
+}
+
+
+function apiCall() {
+    try {
+        request = makeHttpObject()
+        url = generateAPIUrl(baseUrl, accountSid, type);
+        if (type == "call") {
+            payload = createCallPayload({ "From": pfromNumber, "To": ptoNumber, "CallerId": pcallerId, "Record": recording })
+        }
+        request.open('POST', url, false)
+        request.setRequestHeader('Content-Type', ' application/x-www-form-urlencoded');
+
+        request.onreadystatechange = function () {
+            if (request.readyState === 4) {
+                if (request.status == 200) {
+                    try {
+                        var response = JSON.parse(request.responseText);
+                        var check = processResponse(response);
+                        if (check == true) {
+                            goToNextField(true);
+                        }
+                    }
+                    catch {
+                        setResult("danger", "Failure", "Error occured while parsing response")
+                    }
+                }
+                else if (request.status == 400) {
+                    var errorResponse = JSON.parse(request.responseText);
+                    var errorMessage = parseError(errorResponse);
+                    setResult("danger", "Failure", errorMessage);
+                }
+                else if (request.status == 401) {
+                    setResult("danger", "Failure", "Server returned 401, please check plugin credentials")
+                }
+                else if (request.status == 403) {
+                    var errorResponse = JSON.parse(request.responseText);
+                    var errorMessage = parseError(errorResponse);
+                    setResult("danger", "Failure", errorMessage);
+                }
+                else if (request.status == 404) {
+                    setResult("danger", "Failure", "Server returned 404")
+                }
+                else if (request.status == 500) {
+                    setResult("danger", "Failure", "Server returned 500")
+                }
+                else if (request.status == 429) {
+                    setResult("danger", "Failure", "Rate limited, please try again in some time!")
+                }
+                else {
+                    setResult("danger", "Failure", request.responseText)
+                }
+            }
+        }
+        request.onloadstart = function () {
+            dialBtn.disabled = true
+        }
+        request.onloadend = function () {
+            dialBtn.disabled = false
+        }
+        request.onerror = function () {
+            setResult("danger", "Failure", "Network Error, please check your internet connection!")
+        }
+
+        request.send(jsonToQueryString(payload));
+    } catch (error) {
+        setResult("danger", "Failure", error);
+    }
+}
+
 function getSite() {
     var request = makeHttpObject();
     var sresponse;
@@ -65,14 +275,14 @@ function getSite() {
     request.onreadystatechange = function () {
         if (request.readyState === 4) {
             sresponse = request.responseText;
-            exotelResultsValue.value = sresponse;
+            // exotelResultsValue.value = sresponse;
         }
     };
     var urlstring = "https://" + apikey + ":" + apitoken + "@api.exotel.in/v1/Accounts/" + accountSid + "/Calls/connect";
     var params = "";
     if (recording === 0) {
         params = "From=0" + pfromNumber + "&To=0" + ptoNumber + "&CallerId=0" + calledID + "&Record=false";
-	} else {
+    } else {
         params = "From=0" + pfromNumber + "&To=0" + ptoNumber + "&CallerId=0" + calledID + "&Record=true";
     }
     if (type) {
@@ -82,7 +292,7 @@ function getSite() {
         }
     }
     request.open('POST', urlstring, true);
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.setRequestHeader("Content-type", "application/json");
     request.withCredentials = true;
     request.send(params);
 
